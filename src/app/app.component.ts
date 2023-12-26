891cb088bd477404636cf88416a9daeadca45e2f
Vitalii Shapoval <vitalii.sh@walnut.io>
2023-12-26 13:09:53 +0200

    add initial chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Highcharts from 'highcharts';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HighchartsChartModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hc-angular';
+
+  Highcharts: typeof Highcharts = Highcharts;
+
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        type: 'line',
+        data: [1, 2, 3, 4, 5, 6, 10, 1],
+      },
+      {
+        type: "bar",
+        data: [3, 3, 10, 4, 10, 6, 7, 1],
+      }
+    ],
+  };
 }
